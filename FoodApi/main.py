5a0dfef074712879540c9a18cf10cd7619c4223c
Duncan Leng <duncan.leng@ascent.io>
2023-06-06 18:09:38 +0100

    refactor app

--- FoodApi/main.py
+++ FoodApi/main.py
@@ -1,91 +1,71 @@
-import azure.functions as func
-from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
-import random
-import os
+# import azure.functions as func
+from flask import Flask, request, jsonify
+import numpy as np
 
 
 app = Flask(__name__)
-api = Api(app)
-
-food_put_parser = reqparse.RequestParser()
-food_put_parser.add_argument(
-    "food_item", type=str, help="Please supply a food item name", required=True
-)
-
-food_delete_parser = reqparse.RequestParser()
-food_delete_parser.add_argument(
-    "food_item", type=str, help="Please supply a food item name", required=True
-)
-
-foods = {
-    "soup": [
-        "tomato basil soup",
-        "chicken noodle soup",
-        "cauliflower parmesan soup",
-        "meatball soup",
-        "beef coriander",
-        "parsnip soup",
-    ],
-    "pasta": [
-        "pesto pasta",
-        "broccoli carbonara",
-        "bolognese",
-        "tomato pasta",
-        "prawn tomato pasta",
-        "sausage fennel pasta",
-    ],
-    "stew": [
-        "chickpea chorizo stew",
-        "pork beans kale stew",
-        "lentil stew",
-        "beef bouginon",
-    ],
-}
 
 
-def abort_if_food_type_not_in_foods(food_type):
-    if food_type not in foods:
-        abort(404, message="Could not find food type...")
+foods = [
+    {"id":1, "name": "tomato basil soup", "tags": ["soup"]},
+    {"id":2, "name": "chicken noodle soup", "tags": ["soup"]},
+    {"id":3, "name": "cauliflower parmesan soup", "tags": ["soup"]},
+    {"id":4, "name": "meatball soup", "tags": ["soup"]},
+    {"id":5, "name": "beef coriander", "tags": ["soup"]},
+    {"id":6, "name": "parsnip soup", "tags": ["soup"]},
+    {"id":7, "name": "pesto pasta", "tags": ["pasta"]},
+    {"id":8, "name": "broccoli carbonara", "tags": ["pasta"]},
+    {"id":9, "name": "bolognese", "tags": ["pasta"]},
+    {"id":10, "name": "tomato pasta", "tags": ["pasta"]},
+    {"id":11, "name": "sausage fennel pasta", "tags": ["pasta"]},
+    {"id":12, "name": "chickpea chorizo stew", "tags": ["stew"]},
+    {"id":13, "name": "pork beans kale stew", "tags": ["stew"]},
+    {"id":14, "name": "lentil stew", "tags": ["stew"]},
+    {"id":15, "name": "beef bouginon", "tags": ["stew"]},
+]
 
 
-def abort_if_food_item_already_in_food_type_in_foods(food_type, food_item):
-    if food_item in foods.get(food_type, []):
-        abort(409, message=f"{food_item} already exists in {food_type} food type...")
+@app.route('/', methods=['GET'])
+def home():
+    welcome_text = """
+    <h1>My Food API</h1>
+    <p>Welcome to my Food API. A flask API to return food suggestions.</p>
+    <br>
+    <p>Visit "/api/food/all" to view all the food options, or query the </p>
+    <p>data using "/api/food?query=your_query_term&n=number_of_responses".</p>
+    """
+    return welcome_text
+
+@app.route('/api/food/all', methods=['GET'])
+def api_all():
+    return jsonify(foods)
 
 
-def abort_if_food_item_not_in_food_type_in_foods(food_type, food_item):
-    if food_item not in foods[food_type]:
-        abort(404, message=f"{food_item} not found in {food_type} food type...")
+@app.route('/api/food', methods=['GET'])
+def api_query():
+    if 'query' in request.args:
+        query = request.args['query']
+    else:
+        return "Error: No query field provided. Please specify an query.", 404
+    
+    if 'n' in request.args:
+        n = int(request.args['n'])
+    else:
+        n = 1
+
+    results = []
+    for food in foods:
+        if query in food["tags"]:
+            results.append(food)
+    
+    if n > len(results):
+        return "Error: not enough food items to return request", 404
+    
+    results = list(np.random.choice(results, size=n, replace=False))
+
+    return jsonify(results)
 
 
-class Food(Resource):
-    def get(self, food_type, num=1):
-        abort_if_food_type_not_in_foods(food_type)
-        if num > 0:
-            random_food = random.choices(foods[food_type], k=num)
-        elif num == 0:
-            random_food = foods[food_type]
-        else:
-            abort(400, message="Number provided was not a valid input...")
-        return {"data": f"Food: {random_food}"}
-
-    def put(self, food_type):
-        args = food_put_parser.parse_args()
-        abort_if_food_item_already_in_food_type_in_foods(food_type, args["food_item"])
-        foods[food_type] = foods.get(food_type, []) + [args["food_item"]]
-        return {"args": args}, 201
-
-    def delete(self, food_type):
-        args = food_delete_parser.parse_args()
-        abort_if_food_item_not_in_food_type_in_foods(food_type, args["food_item"])
-        foods[food_type].remove(args["food_item"])
-        return "", 204
-
-
-api.add_resource(
-    Food, "/food/<string:food_type>", "/food/<string:food_type>/<int:num>",
-)
 
 if __name__ == "__main__":
     app.run(debug=True)
